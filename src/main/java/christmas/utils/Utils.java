0f66263a9a3eb629bc487e4abac55312eabf444b
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-11-12 23:36:58 +0900

    feat: 문자열을 '-'로 분리하여 나열하는 기능

--- src/main/java/christmas/utils/Utils.java
+++ src/main/java/christmas/utils/Utils.java
@@ -1,5 +1,17 @@
 package christmas.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Utils {
+	
+	public static List<String[]> splitStringsToList(String[] inputs) {
+        List<String[]> splitedInputs = new ArrayList<>();
+        for (String input : inputs) {
+            String[] keyValue = input.split("-");
+            splitedInputs.add(keyValue);
+        }
+        return splitedInputs;
+    }
 
 }
--- src/main/java/christmas/validation/Validation.java
+++ src/main/java/christmas/validation/Validation.java
@@ -8,4 +8,5 @@ public class Validation {
 			throw new IllegalArgumentException();
 		}
 	}
+
 }
