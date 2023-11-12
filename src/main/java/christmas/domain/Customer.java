3602533af19ca593ef0ec43389b4e23fdf79367a
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-11-13 00:28:32 +0900

    feat: 주문된 총 메뉴수가 20개 초과일 경우 예외 처리하는 기능 추가

--- src/main/java/christmas/domain/Customer.java
+++ src/main/java/christmas/domain/Customer.java
@@ -2,12 +2,15 @@ package christmas.domain;
 
 import java.util.List;
 
+import christmas.validation.Validation;
+
 public class Customer {
 	
 	private final int date;
 	private final List<String[]> orders;
 	
 	Customer(int date, List<String[]> orders) {
+		validateOrders(orders);
 		this.date = date;
 		this.orders = orders;
 	}
@@ -18,6 +21,12 @@ public class Customer {
 	
 	public List<String[]> getOrders() {
 		return orders;
+	}
+	
+	private void validateOrders(List<String[]> orders) {
+		Validation.validateOrderMenu(orders);
+		Validation.validateMoreThanOneMenu(orders);
+		Validation.validateDistinctOrder(orders);
 	}
 
 }
--- src/main/java/christmas/validation/Validation.java
+++ src/main/java/christmas/validation/Validation.java
@@ -3,6 +3,7 @@ package christmas.validation;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import christmas.domain.Menu;
@@ -12,15 +13,16 @@ public class Validation {
 	private static final int MENU_INDEX = 0;
 	private static final int COUNT_INDEX = 1;
 	private static final int MIN_ORDER_COUNT = 1;
+	private static final int MAX_ORDER_COUNT = 20;
 	
-	public void validateInputs(String[] inputs) {
+	public static void validateInputs(String[] inputs) {
 		for(String input : inputs)
 		if (!input.matches("\\S+-\\d+")) {
 			throw new IllegalArgumentException();
 		}
 	}
 	
-	public void validateOrderMenu(List<String[]> splitedInputs) {  //메뉴판에 없는 메뉴일 경우 예외처리
+	public static void validateOrderMenu(List<String[]> splitedInputs) {  //메뉴판에 없는 메뉴일 경우 예외처리
         for (String[] splitedInput : splitedInputs) {
         	if(Arrays.stream(Menu.values()).anyMatch(menu -> menu.getViewName() == splitedInput[MENU_INDEX])) {
         		throw new IllegalArgumentException();
@@ -28,7 +30,7 @@ public class Validation {
         }
 	}
 	
-	public void validateOrderCount(List<String[]> splitedInputs) {
+	public static void validateMoreThanOneMenu(List<String[]> splitedInputs) {
         for (String[] splitedInput : splitedInputs) {
         	if(Integer.valueOf(splitedInput[COUNT_INDEX]) < MIN_ORDER_COUNT) {
         		throw new IllegalArgumentException();
@@ -36,12 +38,22 @@ public class Validation {
         }
 	}
 	
-	public void validateDistinctOrder(List<String[]> splitedInputs) {
+	public static void validateDistinctOrder(List<String[]> splitedInputs) {
 		Set<String> menuInput = new HashSet<>();
 		for (String[] splitedInput : splitedInputs) {
 			menuInput.add(splitedInput[MENU_INDEX]);
 		}
 		if(menuInput.size() != splitedInputs.size()) {
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	public static void validateTotalMenuCount(Map<Menu, Integer> orders) {
+		int count = 0;
+		for (Menu menu : orders.keySet()) {
+			count += orders.get(menu);
+		}
+		if(count > MAX_ORDER_COUNT) {
 			throw new IllegalArgumentException();
 		}
 	}
