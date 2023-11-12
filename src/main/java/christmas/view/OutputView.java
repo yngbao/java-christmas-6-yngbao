167ab2b2d3cad802e7a874f298f7f8d1d99ba080
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-11-12 19:30:23 +0900

    feat: 인삿말, 미리보기 안내, 주문메뉴 출력 기능

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,24 @@
 package christmas.view;
 
+import christmas.domain.Counter;
+import christmas.domain.Event;
+import christmas.domain.Menu;
+
 public class OutputView {
+	
+	public void printHello() {
+		System.out.printf("안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다.\n", Event.EVENT_MONTH);
+	}
+	
+	public void printPreviewIntro(int date) {
+		System.out.printf("%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n\n", Event.EVENT_MONTH, date);
+	}
+	
+	public void printOrders() {
+		System.out.println("<주문 메뉴>");
+		for(Menu menu : Counter.getOrders().keySet()) {
+			System.out.println(menu.getViewName()+" "+Counter.getOrders().get(menu)+"개");
+		}
+	}
 
 }
