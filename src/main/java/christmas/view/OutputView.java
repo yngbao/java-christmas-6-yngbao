dd0f5aac16388809561a74c1a18a8a24938ddcd9
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-11-12 19:42:38 +0900

    feat: 할인 전 총 주문금액 출력 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,24 +1,33 @@
 package christmas.view;
 
+import java.text.DecimalFormat;
+
 import christmas.domain.Counter;
 import christmas.domain.Event;
 import christmas.domain.Menu;
 
 public class OutputView {
 	
+	DecimalFormat formatter = new DecimalFormat("###,###원");
+	
 	public void printHello() {
 		System.out.printf("안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다.\n", Event.EVENT_MONTH);
 	}
 	
 	public void printPreviewIntro(int date) {
-		System.out.printf("%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n\n", Event.EVENT_MONTH, date);
+		System.out.printf("%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n", Event.EVENT_MONTH, date);
 	}
 	
 	public void printOrders() {
-		System.out.println("<주문 메뉴>");
+		System.out.println("\n<주문 메뉴>");
 		for(Menu menu : Counter.getOrders().keySet()) {
 			System.out.println(menu.getViewName()+" "+Counter.getOrders().get(menu)+"개");
 		}
+	}
+	
+	public void printTotalOrderAmount() {
+		System.out.println("\n<할인 전 총주문 금액>\n"
+				+formatter.format(Counter.getTotalOrderAmount()));
 	}
 
 }
