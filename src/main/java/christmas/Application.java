f6fde43caa820db8b970fd9195300840050fcd6a
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-11-13 21:19:01 +0900

    feat(Application): 어플리케이션 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,13 @@
 package christmas;
 
+import christmas.controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
+    	
+    	Controller controller = new Controller();
+    	
+    	controller.run();
         
     }
 }
