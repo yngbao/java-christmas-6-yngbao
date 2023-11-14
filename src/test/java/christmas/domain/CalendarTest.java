6e5ddc82398a203efac001f144e0c018451cc2c3
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-11-14 12:12:48 +0900

    test: Calendar의 각 메서드 기능 테스트 추가

--- src/test/java/christmas/domain/CalendarTest.java
+++ src/test/java/christmas/domain/CalendarTest.java
@@ -8,11 +8,27 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class CalendarTest {
 
-	@DisplayName("입력된 날짜의 요일을 구한다.")
+	@DisplayName("입력된 날짜가 평일(일~목)인지 아닌지 판단한다.")
 	@CsvSource(value = {"16:false", "25:true"}, delimiter = ':')
     @ParameterizedTest
-    void IsWeekday(Integer date, boolean isWeekday) {
+    void isWeekdayTest(Integer date, boolean isWeekday) {
 		boolean actual = Calendar.isWeekday(date);
+        assertThat(actual).isEqualTo(isWeekday);
+    }
+	
+	@DisplayName("입력된 날짜가 크리스마스 디데이 이벤트 대상인지 아닌지 판단한다.")
+	@CsvSource(value = {"2:true", "18:true", "26:false"}, delimiter = ':')
+    @ParameterizedTest
+    void isBeforeChristmasTest(Integer date, boolean isWeekday) {
+		boolean actual = Calendar.isBeforeChristmas(date);
+        assertThat(actual).isEqualTo(isWeekday);
+    }
+	
+	@DisplayName("입력된 날짜가 특별할인 대상인지 아닌지 판단한다.")
+	@CsvSource(value = {"3:true", "10:true", "17:true", "24:true", "25:true", "31:true", "15:false"}, delimiter = ':')
+    @ParameterizedTest
+    void isSpecialTest(Integer date, boolean isWeekday) {
+		boolean actual = Calendar.isSpecial(date);
         assertThat(actual).isEqualTo(isWeekday);
     }
 
