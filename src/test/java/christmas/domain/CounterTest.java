96650b9771f71b3bb0f7a9815829e3a403d961f9
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-11-14 14:26:17 +0900

    test: CounterTest 셋업 코드 추가하여 수정

--- src/test/java/christmas/domain/CounterTest.java
+++ src/test/java/christmas/domain/CounterTest.java
@@ -2,29 +2,41 @@ package christmas.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CounterTest {
+	private static Map<Menu, Integer> customerOrder = new HashMap<>();
+	
+	@BeforeAll
+	static void setUp() {
+		customerOrder.put(Menu.CAESAR_SALAD, 1);
+		customerOrder.put(Menu.SEAFOOD_PASTA, 1);
+		customerOrder.put(Menu.ICECREAM, 2);
+		customerOrder.put(Menu.ZERO_COKE, 1);
+	}
 
 	@DisplayName("주문된 메뉴를 주문 목록에 저장한다.")
     @Test
     void storeOrderTest() {
 		Counter counter = new Counter();
-		counter.storeOrder(Menu.CAESAR_SALAD, 1);
+		counter.storeOrder(customerOrder);
 
-        assertThat(counter.getOrders()).containsEntry(Menu.CAESAR_SALAD, 1);
+        assertThat(counter.getOrders()).containsAllEntriesOf(customerOrder);
     }
 	
 	@DisplayName("주문목록의 총 합계금액을 구한다.")
     @Test
     void getTotalOrderAmount() {
 		Counter counter = new Counter();
-		counter.storeOrder(Menu.SEAFOOD_PASTA, 1);
-		counter.storeOrder(Menu.ICECREAM, 2);
+		counter.storeOrder(customerOrder);
         int result = counter.getTotalOrderAmount();
 
-        assertThat(result).isEqualTo(45000);
+        assertThat(result).isEqualTo(56000);
     }
 
 }
